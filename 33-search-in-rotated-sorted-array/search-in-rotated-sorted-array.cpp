fba5f75587cc05d8de14296d3e2f01a46875aee8
DHAIRYA CHADHA <dhairyachadha2002@gmail.com>
2026-08-26 11:09:56 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-
         int start = 0, end = nums.size() - 1;
-
         while(start <= end) {
-
             int mid = start + (end - start) / 2;
-
             if(nums[mid] == target) {
                 return mid;
             }
-
-            // Left half is sorted
             if(nums[start] <= nums[mid]) {
 
                 if(nums[start] <= target && target < nums[mid]) {
@@ -22,10 +16,7 @@ public:
                     start = mid + 1;
                 }
             }
-
-            // Right half is sorted
             else {
-
                 if(nums[mid] < target && target <= nums[end]) {
                     start = mid + 1;
                 }
@@ -34,7 +25,6 @@ public:
                 }
             }
         }
-
         return -1;
     }
 };
